47f5b7acb13e86b243a35da1837119e240ddadb6
Michael Bestas <mikeioannina@gmail.com>
2015-04-22 02:15:28 +0300

    SystemUI: Add heads up tile
    Change-Id: I0e3cff73752c59d804dcec43b86524c2e8df4f57
    
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Change-Id: Idc09a041662ba35bb1a9e03352368154cf529a10

--- packages/SystemUI/src/com/android/systemui/fortune/FortuneOSModule.kt
+++ packages/SystemUI/src/com/android/systemui/fortune/FortuneOSModule.kt
@@ -7,6 +7,7 @@ package com.android.systemui.fortune
 
 import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.PowerShareTile
+import com.android.systemui.qs.tiles.HeadsUpTile
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -20,4 +21,10 @@ interface FortuneOSModule {
     @IntoMap
     @StringKey(PowerShareTile.TILE_SPEC)
     fun bindPowerShareTile(powerShareTile: PowerShareTile): QSTileImpl<*>
+
+    /** Inject HeadsUpTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(HeadsUpTile.TILE_SPEC)
+    fun bindHeadsUpTile(headsUpTile: HeadsUpTile): QSTileImpl<*>
 }
