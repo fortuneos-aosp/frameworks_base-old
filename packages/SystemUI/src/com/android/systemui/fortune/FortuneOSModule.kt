8d3787976accac05696af50634c8d2814976fbe0
Keith Mok <kmok@cyngn.com>
2016-02-04 11:10:22 -0800

    SystemUI: Add caffeine QS tile
    Co-authored-by: chillger <alexander.loskutov@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: I0b9a498285930cc397b7c4d20ddec3f447e6d332

--- packages/SystemUI/src/com/android/systemui/fortune/FortuneOSModule.kt
+++ packages/SystemUI/src/com/android/systemui/fortune/FortuneOSModule.kt
@@ -7,6 +7,7 @@ package com.android.systemui.fortune
 
 import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.AODTile
+import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.PowerShareTile
 import com.android.systemui.qs.tiles.HeadsUpTile
@@ -25,6 +26,12 @@ interface FortuneOSModule {
     @StringKey(AODTile.TILE_SPEC)
     fun bindAODTile(aodTile: AODTile): QSTileImpl<*>
 
+    /** Inject CaffeineTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(CaffeineTile.TILE_SPEC)
+    fun bindCaffeineTile(caffeineTile: CaffeineTile): QSTileImpl<*>
+    
     /** Inject CellularTile into tileMap in QSModule */
     @Binds
     @IntoMap
