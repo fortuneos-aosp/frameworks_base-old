a7ff925e3ffbf81817bcc7b70520297b64b19339
Chris Crump <chris@cypheros.co>
2019-10-05 00:13:58 -0400

    core: Utils: Add method to check udfps support
    Change-Id: I65392ddde1f30b34337f255d5a7229a757961485

---
+++ core/java/com/android/internal/util/fortune/udfps/UdfpsUtils.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2024 FortuneOS
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.internal.util.fortune.udfps;
+
+import android.content.Context;
+
+import com.android.internal.util.ArrayUtils;
+
+public class UdfpsUtils {
+    public static boolean hasUdfpsSupport(Context context) {
+        int[] udfpsProps = context.getResources().getIntArray(
+                com.android.internal.R.array.config_udfps_sensor_props);
+
+        return !ArrayUtils.isEmpty(udfpsProps);
+    }
+}
