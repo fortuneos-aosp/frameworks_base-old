2cad886dfde9910caf894a38234d279b09a1b0c5
Ido Ben-Hur <idoybh2@gmail.com>
2022-11-21 16:48:45 +0200

    base: Allow customizing default notification vibration pattern [1/2]
    Re-use the patterns we've got for ringtone vibration as options
    In addition allow crafting a custom pattern same as we do for ringtone
    
    Change-Id: I6faba1966f6d9b7ec1c057a7b214d35207efd4e1
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -26,6 +26,7 @@ import static android.provider.settings.validators.SettingsValidators.NON_NEGATI
 import static android.provider.settings.validators.SettingsValidators.NON_NEGATIVE_INTEGER_VALIDATOR;
 import static android.provider.settings.validators.SettingsValidators.URI_VALIDATOR;
 import static android.provider.settings.validators.SettingsValidators.VIBRATION_INTENSITY_VALIDATOR;
+import static android.provider.settings.validators.SettingsValidators.CUSTOM_VIBRATION_PATTERN_VALIDATOR;
 
 import android.annotation.Nullable;
 import android.compat.annotation.UnsupportedAppUsage;
@@ -248,21 +249,8 @@ public class SystemSettingsValidators {
         VALIDATORS.put(System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.SWIPE_TO_SCREENSHOT, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.RINGTONE_VIBRATION_PATTERN, new InclusiveIntegerRangeValidator(0, 5));
-        VALIDATORS.put(System.CUSTOM_RINGTONE_VIBRATION_PATTERN,
-                new Validator() {
-                    @Override
-                    public boolean validate(String value) {
-                        String[] args = value.split(",", 0);
-                        if (args.length != 3) return false;
-                        try {
-                            for (String str : args)
-                                if (Integer.parseInt(str) < 0)
-                                    return false;
-                        } catch (NumberFormatException e) {
-                            return false;
-                        }
-                        return true;
-                    }
-                });
+        VALIDATORS.put(System.CUSTOM_RINGTONE_VIBRATION_PATTERN, CUSTOM_VIBRATION_PATTERN_VALIDATOR);
+        VALIDATORS.put(System.NOTIFICATION_VIBRATION_PATTERN, new InclusiveIntegerRangeValidator(0, 5));
+        VALIDATORS.put(System.CUSTOM_NOTIFICATION_VIBRATION_PATTERN, CUSTOM_VIBRATION_PATTERN_VALIDATOR);
     }
 }
