da3f150cc2d3c6e41512431ab35faf8cbfa3f629
Rituj Beniwal <ritujbeniwal@gmail.com>
2023-06-13 22:52:41 +0200

    base: Add custom vibration patterns [1/3] [SQUASH]
    idoybh edits: Add settings value to backup array
    
    Squashed with:
    
      base: Allow customizing default notification vibration pattern [1/2]
    
        Re-use the patterns we've got for ringtone vibration as options
        In addition allow crafting a custom pattern same as we do for ringtone
    
        Change-Id: Id22be7675a5c0746a8d4fa0ed543fd80ff39f3fd
    
      base: Add different custom notification vibration pattern [1/2]
    
        * the ones used by calls don't really fit here
    
        Change-Id: Ib89d5421dc4055176f0ae15186da98b359d8f7c1
    
      base: Add different custom notification vibration pattern [1/2] (2)
    
        Change-Id: I3a82b5275c48f322c0b96f22a14bccada2d9c2df
    
    Change-Id: I98d870f917f0470c14390d35c3ed08de2f8232a6
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Co-authored-by: Vishalcj17 <vishalcj@aospa.co>

--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -254,5 +254,7 @@ public class SystemSettingsValidators {
         VALIDATORS.put(System.INCREASING_RING, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.INCREASING_RING_START_VOLUME, new InclusiveFloatRangeValidator(0, 1));
         VALIDATORS.put(System.INCREASING_RING_RAMP_UP_TIME, new InclusiveFloatRangeValidator(5, 60));
+        VALIDATORS.put(System.RINGTONE_VIBRATION_PATTERN, new InclusiveIntegerRangeValidator(0, 4));
+        VALIDATORS.put(System.NOTIFICATION_VIBRATION_PATTERN, new InclusiveIntegerRangeValidator(0, 4));
     }
 }
