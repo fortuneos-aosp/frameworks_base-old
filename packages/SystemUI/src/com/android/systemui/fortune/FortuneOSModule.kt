a85424d1c5397af03f61001af2a297d15b99d386
Michael Bestas <mkbestas@gmail.com>
2024-03-25 20:44:03 +0100

    Reapply "SystemUI: Restore wifi and cellular QS tiles"
    This reverts commit f2f88daa84b805a047c24a4a99381aecb1e087b1.
    
    Change-Id: I3cc828d03c6657b9865c62e19dddab1416015a60

--- packages/SystemUI/src/com/android/systemui/fortune/FortuneOSModule.kt
+++ packages/SystemUI/src/com/android/systemui/fortune/FortuneOSModule.kt
@@ -7,9 +7,11 @@ package com.android.systemui.fortune
 
 import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.CaffeineTile
+import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
+import com.android.systemui.qs.tiles.WifiTile
 
 import dagger.Binds
 import dagger.Module
@@ -24,6 +26,12 @@ interface FortuneOSModule {
     @IntoMap
     @StringKey(CaffeineTile.TILE_SPEC)
     fun bindCaffeineTile(caffeineTile: CaffeineTile): QSTileImpl<*>
+
+    /** Inject CellularTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(CellularTile.TILE_SPEC)
+    fun bindCellularTile(cellularTile: CellularTile): QSTileImpl<*>
 
     /** Inject HeadsUpTile into tileMap in QSModule */
     @Binds
@@ -42,4 +50,10 @@ interface FortuneOSModule {
     @IntoMap
     @StringKey(UsbTetherTile.TILE_SPEC)
     fun bindUsbTetherTile(usbTetherTile: UsbTetherTile): QSTileImpl<*>
+
+    /** Inject WifiTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(WifiTile.TILE_SPEC)
+    fun bindWifiTile(wifiTile: WifiTile): QSTileImpl<*>
 }
